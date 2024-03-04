23d43fe28985aa01f08bb96ed5557acc32c4c888
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2024-03-04 20:46:46 +0900

    test: mock sentry_sdk.init to improve code coverage

--- test/test_sentry.py
+++ test/test_sentry.py
@@ -2,6 +2,7 @@
 
 import logging
 import unittest
+import unittest.mock
 
 import boilerplates.sentry
 
@@ -13,3 +14,21 @@ class SentryTests(unittest.TestCase):
             boilerplates.sentry.Sentry.init()
         self.assertEqual(len(context.output), 1, msg=context.output)
         self.assertIn('skipping Sentry SDK initialisation', context.output[0])
+
+    def test_init_without_dsn(self):
+        class Sentry(boilerplates.sentry.Sentry):
+            dsn = ''
+        with self.assertLogs('boilerplates.sentry', logging.INFO) as context, \
+                unittest.mock.patch('sentry_sdk.init') as sentry_sdk_init_mock:
+            Sentry.init()
+        sentry_sdk_init_mock.assert_not_called()
+        self.assertEqual(len(context.output), 1, msg=context.output)
+        self.assertIn('skipping Sentry SDK initialisation', context.output[0])
+
+    def test_init_with_dsn(self):
+        class Sentry(boilerplates.sentry.Sentry):
+            dsn = 'https://spam@ham.ingest.sentry.io/eggs'
+        with self.assertNoLogs('boilerplates.sentry', logging.INFO), \
+                unittest.mock.patch('sentry_sdk.init') as sentry_sdk_init_mock:
+            Sentry.init()
+        sentry_sdk_init_mock.assert_called_once()
